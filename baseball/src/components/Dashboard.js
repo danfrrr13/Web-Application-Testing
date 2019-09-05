3face0387dce73b5992106ebe26465fd83c17497
Daniel Ferrer <danfrrr13@gmail.com>
2019-09-05 17:06:38 -0400

    MVP functionality and styling completed

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Dashboard = props => {
-    const ball = e => {
-        e.preventDefault();
+    useEffect(() => {
+        if (props.values.ball > 3) {
+            reset();
+        }
+    }, [props.values.ball]);
+    useEffect(() => {
+        if (props.values.strike > 2) {
+            reset();
+        }
+    }, [props.values.strike]);
+    const ball = () => {
         props.setValues(prevValues => ({...prevValues, ball: prevValues.ball + 1}));
-      }
-    const strike = e => {
-        e.preventDefault();
+        
+    }
+    const strike = () => {
         props.setValues(prevValues => ({...prevValues, strike: prevValues.strike + 1}));
-      }
-    const hit = e => {
-        e.preventDefault();
+    }
+    const hit = () => {
         props.setValues(prevValues => ({...prevValues, hit: prevValues.hit + 1}));
-      }
-    const foul = e => {
-        e.preventDefault();
+        reset();
+    }
+    const foul = () => {
         props.setValues(prevValues => ({...prevValues, foul: prevValues.foul + 1}));
         if (props.values.strike === 0 || props.values.strike === 1) {
           props.setValues(prevValues => ({...prevValues, strike: prevValues.strike + 1}));
         } 
-      }
-    const reset = e => {
-        e.preventDefault();
+    }
+    const reset = () => {
         props.setValues({
           ball: 0,
           strike: 0,
@@ -30,12 +37,13 @@ const Dashboard = props => {
         })
       }
     return (
-        <div>
-            <button onClick={ball}>Ball</button>
-            <button onClick={strike}>Strike</button>
-            <button onClick={hit}>Hit</button>
-            <button onClick={foul}>Foul</button>
-            <button onClick={reset}>Reset</button>
+        <div className='dashboard'>
+            <div className='mainButtons'>
+                <button onClick={ball}>Ball</button>
+                <button onClick={strike}>Strike</button>
+                <button onClick={hit}>Hit</button>
+                <button onClick={foul}>Foul</button>
+            </div>
         </div>
     )
 }
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Display = props => {
     return (
-        <div>
-            <p>Ball: {props.values.ball} Strike: {props.values.strike} </p>
+        <div className='display'>
+            <div className='displaySquare'>
+                <h3>
+                    Ball:
+                </h3>
+                <h1>
+                    {props.values.ball}
+                </h1>
+            </div>
+            <div className='displaySquare'>
+                <h3>
+                    Strike:
+                </h3>
+                <h1>
+                    {props.values.strike}
+                </h1>
+            </div>
         </div>
     )
 }
